f05a066bbe74982639267693148174b3032c25f9
OlyaTru <rasokha.volha@gmail.com>
2024-08-12 16:25:32 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,32 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '*****': ' ',
 };
 
+const DIGIT_TABLE = {
+    '00': '',
+    '10': '.',
+    '11': '-',
+    '**': '*',
+}
+
 function decode(expr) {
-    // write your solution here
+    let resultExpression = [];
+    let resultDotsAndDashes10 = [];
+    let resultDotsAndDashesAll = [];
+    for (let start = 0; start < expr.length; start += 10) {
+        let expr10 = expr.substr(start, 10);
+        resultDotsAndDashes10 = [];
+        for (let start = 0; start < 10; start += 2) {
+            resultDotsAndDashes10.push(DIGIT_TABLE[expr10.substr(start, 2)]);
+        }
+        resultDotsAndDashesAll.push(resultDotsAndDashes10.join(''));
+    }
+    for (let i = 0; i < resultDotsAndDashesAll.length; i++) {
+        resultExpression.push(MORSE_TABLE[resultDotsAndDashesAll[i]]);
+    }
+    return resultExpression.join('');
 }
 
 module.exports = {
